2c2e36e39bbaa21a2dfc08e916c043e0d35e8204
Luan Souza <araujo_luan2001@outlook.com.br>
2022-04-24 17:58:15 -0300

    testando o .interruped(), mas sem sucesso

--- fundamentos/Threads/src/SleepMessages.java
+++ fundamentos/Threads/src/SleepMessages.java
@@ -1,5 +1,5 @@
 public class SleepMessages {
-    public static void main(String [] args) throws InterruptedException{
+    public static void main(String [] args) /*throws InterruptedException*/{
 
             String importantInfos [] = {
                     "Eu gosto do Rubens",
@@ -8,11 +8,26 @@ public class SleepMessages {
                     "Eu quero um abraço dele agora"
             };
 
-            for(int i = 0; i < importantInfos.length; i++) {
+                for (int i = 0; i < importantInfos.length; i++) {
 
-                //1 milisegundo é 10^-3 segundos, 4000 milisegundos é 4 segundos
-                Thread.sleep(4000);
-                System.out.println(importantInfos[i]);
-            }
+
+                    //1 milisegundo é 10^-3 segundos, 4000 milisegundos é 4 segundos
+
+                    try{
+                        Thread.sleep(2000);
+
+                    } catch (InterruptedException e){
+
+                        if (Thread.interrupted()) {
+
+                            //aqui ele verifica se o processo foi interrompido (APENAS PARA VERIFICAR MESMO)
+                            //NAO EXIBE A MENSAGEM (???)
+                            System.out.println("\n\tVerificou se a Thread estava interrompida");
+                        }
+                    }
+
+                    System.out.println(importantInfos[i]);
+                }
+
     }
 }
