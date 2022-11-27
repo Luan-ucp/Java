11abb51c4f3e4c70e376e42721fe0af2d7c685ef
Luan Souza <araujo_luan2001@outlook.com.br>
2022-11-27 16:54:18 -0300

    removido pasta /fundamentos

--- fundamentos/Threads/src/HelloRunnable.java
+++
@@ -1,9 +0,0 @@
-public class HelloRunnable implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("Hello from a Threads");
-    }
-    public static void main(String[] args){
-        (new Thread(new HelloRunnable())).start();
-    }
-}
